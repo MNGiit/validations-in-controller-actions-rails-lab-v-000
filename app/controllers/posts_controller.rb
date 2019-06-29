0ead5cc9ffc281b16795ac1a995bfc764b617c4e
Mohammed Noori <kmanoorib@gmail.com>
2019-06-29 19:30:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
   def update
     # @post.update(post_params)
-    @post = Post.new(post_params)
+    # @post = Post.new(post_params)
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
